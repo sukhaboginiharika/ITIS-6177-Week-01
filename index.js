bbdc4ab0ab3275e061a1ad9256de0b01617f2e87
Sukhabogi Niharika <sukhaboginiharika@Sukhabogis-MacBook-Air.local>
2022-01-17 12:25:26 -0500

    Hello

---
+++ index.js
@@ -0,0 +1,5 @@
+var http = require("http");
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World test\n');
+}).listen(8081);
